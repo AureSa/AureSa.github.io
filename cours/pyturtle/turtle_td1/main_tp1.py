ad1dbaa2b6db8a14586298fa162a1f3458cb2be6
AureSa <aurelie.saulq@gmail.com>
2024-02-02 23:45:07 +0100

    ajout cours 1 pyturtle

---
+++ cours/pyturtle/turtle_td1/main_tp1.py
@@ -0,0 +1,38 @@
+from turtle import *
+from time import sleep
+
+# s = getscreen()
+tortue = Turtle()
+
+# tortue.forward(100)
+# tortue.right(90)
+# tortue.forward(100)
+
+# un carré de 100  de coté
+# tortue.forward(100)
+# tortue.right(90)
+# tortue.forward(100)
+# tortue.right(90)
+# tortue.forward(100)
+# tortue.right(90)
+# tortue.forward(100)
+
+# un carré de 150 de côté
+# tortue.forward(150)
+# tortue.right(90)
+# tortue.forward(150)
+# tortue.right(90)
+# tortue.forward(150)
+# tortue.right(90)
+# tortue.forward(150)
+
+# un rectangle de 200 de longueur et de 50 de largeur
+tortue.forward(200)
+tortue.right(90)
+tortue.forward(50)
+tortue.right(90)
+tortue.forward(200)
+tortue.right(90)
+tortue.forward(50)
+
+exitonclick()
